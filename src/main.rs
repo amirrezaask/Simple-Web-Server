2be557dfbbabe962c216ea246c15e3dc232c42ce
amirrezaask <raskarpour@gmail.com>
2019-04-15 18:11:27 +0430

    now our web server serves http

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 extern crate clap;
-use std::collections::HashMap;
 use std::io::prelude::{ Read, Write };
 use std::net::{ TcpListener, TcpStream };
 use clap::{App, ArgMatches, Arg};
+use std::ops::Add;
+use std::fs;
 
-fn handle_stream (mut stream: TcpStream) {
+
+
+fn index (mut stream: TcpStream) {
     let mut buffer = [0; 512];
-    if stream.read(&mut buffer).is_err() {
-        println!("can't read stream" );
-        return
+
+    match stream.read(&mut buffer) {
+        Err(e) => println!("can't read stream : {}",e),
+        _ => (),
     }
+    let template = fs::read_to_string("index.html").unwrap_or(String::from("Could not read template")); 
+    
     println!("Request : {} ", String::from_utf8_lossy(&buffer[..]));
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",template);
+    println!("{}", response);
+    match stream.write(response.as_bytes()) {
+        Ok(n) => println!("#{} bytes written", n),
+        Err(e) => println!("could not write request : {}", e)
+    } 
+    
+    match stream.flush() {
+        Ok(_) => return,
+        Err(e) => println!("Error on flushing stream : {}", e)
+    }
 }
-
-
 
 fn get_port<'a>() -> u32 {
     let matches = App::new("web-server-rs")
@@ -42,6 +58,6 @@ fn main () {
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port_number)).unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_stream(stream);
+        index(stream);
     }
 }
